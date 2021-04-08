17dde8442be8d79a92061f71d98ad9ea49b0cc69
AnkitaSK <ankitakalangutkar90@gmail.com>
2021-04-08 23:23:54 +0200

    Fixes

--- SubSequenceProblemsUsingDP/SubSequenceProblemsUsingDP/CommonSubsequence.swift
+++ SubSequenceProblemsUsingDP/SubSequenceProblemsUsingDP/CommonSubsequence.swift
@@ -72,10 +72,10 @@ func lcsMemoization(_ x: String, _ y: String, _ n: Int, _ m: Int) -> Int {
     let _xIndex = x.index(x.startIndex, offsetBy: n - 1)
     let _yIndex = y.index(y.startIndex, offsetBy: m - 1)
     if x[_xIndex] == y[_yIndex] {
-        t[n][m] = lcs(x, y, n - 1, m - 1) + 1 // + 1 because one match is found
+        t[n][m] = lcsMemoization(x, y, n - 1, m - 1) + 1 // + 1 because one match is found
         return t[n][m]
     } else {
-        t[n][m] = max(lcs(x, y, n - 1, m), lcs(x, y, n, m - 1)) // max because longest
+        t[n][m] = max(lcsMemoization(x, y, n - 1, m), lcsMemoization(x, y, n, m - 1)) // max because longest
         return t[n][m]
     }
 }
@@ -88,18 +88,11 @@ func lcsString() -> String { // not working -- need to debug again
     var outPutString = ""
     var i = n
     var j = m
+    let xArray = Array(x)
+    let yArray = Array(y)
     while i > 0 && j > 0 {
-        var _iIndex = x.index(x.startIndex, offsetBy: i - 1)
-        if i < x.count {
-            _iIndex = x.index(x.startIndex, offsetBy: i)
-        }
-        var _jIndex = y.index(y.startIndex, offsetBy: j - 1)
-        if j < y.count {
-            _jIndex = y.index(x.startIndex, offsetBy: j)
-        }
-        
-        if x[_iIndex] == y[_jIndex] {
-            outPutString += String(x[_iIndex]) // either, because they are equal
+        if xArray[i - 1] == yArray[j - 1] {
+            outPutString.append(String(xArray[i - 1])) // either, because they are equal
             i -= 1
             j -= 1
         } else if t[i - 1][j] > t[i][j - 1] { // we came from max part
